2b785c3def8ac729e43a1dc570cb424446ecc2a2
MustakShan <mustakshan2002@gmail.com>
2021-01-18 23:48:49 +0600

    index.html

---
+++ script.js
@@ -0,0 +1,19 @@
+const myFriends = [`Dip`, `Emon`, `Mahi`, `Anonto`, `Alif`];
+console.log(myFriends);
+console.log(myFriends.length);
+
+const birthYears = [2000, 2002, 2006, 2008, 2010];
+console.log(birthYears);
+console.log(birthYears.length);
+
+const theirProfessions = [`student`, `Freelancer`, `Buisness`, `GAmer`, `Student`];
+console.log(theirProfessions);
+console.log(theirProfessions.length);
+
+for (let myFriends = 0; myFriends <= 5; myFriends++) {
+    console.log(`loop`, myFriends);
+}
+
+for (let theirProfessions = 0; theirProfessions <= 5; theirProfessions++) {
+    console.log(`for loop`, theirProfessions);
+}
